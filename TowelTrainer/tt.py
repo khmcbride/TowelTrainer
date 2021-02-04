faf035c93b6ab5b78f9dd675646a2a91d54aeabc
gkpurcey <gkpurcey@users.noreply.github.com>
2021-02-04 14:40:34 -0600

    Update tt.py

--- TowelTrainer/tt.py
+++ TowelTrainer/tt.py
@@ -131,7 +131,7 @@ class Contraption:
 			if self.passes_remaining == 0:
 				#  Added GKP
 				self.sm_sled.halt_rotation()
-				time.sleep(1)
+				time.sleep(2)
 				#End Added GKP
 				self.stop_all_motors()
 				print('Finished...')
